213de8004a30a7a343bf8eec6e0458fc48a0673a
おつよん <31351395+otyn0308@users.noreply.github.com>
2017-12-28 22:43:09 +0900

    Update a.java

--- abc001/a.java
+++ abc001/a.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ABC_A001{
+public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
