50e0caa9f7faa71dffb23594d438df29f966c51b
おつよん <31351395+otyn0308@users.noreply.github.com>
2017-12-28 23:21:18 +0900

    Create a.java

---
+++ abc002/a.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ABC_A002{
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	int a = sc.nextInt();
+    	int b = sc.nextInt();
+    	if(a > b){
+    		System.out.println(a);
+    	}else if(a < b){
+    		System.out.println(b);
+    	}
+    }
+}
