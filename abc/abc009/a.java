a685f9e749ce3f7f2bb74ff880b387fb88cf2f62
otyn0308 <elements0308@gmail.com>
2017-12-30 21:50:37 +0900

    first commit

---
+++ abc/abc009/a.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+    		int a = sc.nextInt();
+    		if(a % 2 == 0) {
+    			System.out.println(a / 2);
+    		}else {
+    			System.out.println((a + 1) / 2);
+    		}
+	}
+}
