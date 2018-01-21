7f880cd73159e6bfd1866d5109d24d638f137418
otyn0308 <elements0308@gmail.com>
2018-01-21 16:41:57 +0900

    initial commit

---
+++ abc/abc039/a.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+    System.out.println(2*a*b + 2*b*c + 2*c*a);
+  }
+}
