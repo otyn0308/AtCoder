b651f190a57680c40221831c27c95abda1550a91
otyn0308 <elements0308@gmail.com>
2018-04-15 17:22:57 +0900

    abc094

---
+++ abc/abc094/a.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int x = sc.nextInt();
+    if((a+b)>x || a<x){
+      System.out.println("YES");
+    }else{
+      System.out.println("NO");
+    }
+  }
+}
